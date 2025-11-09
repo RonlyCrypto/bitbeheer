a6822664dd08dba848460653ce6bbf7f6f9dd945
RonlyCrypto <giovanni.hooten@gmail.com>
2025-11-09 02:55:53 +0100

    Add DROP TRIGGER IF EXISTS for notification preferences trigger

--- create-notification-preferences-table.sql
+++ create-notification-preferences-table.sql
@@ -86,6 +86,9 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- Drop existing trigger if it exists
+DROP TRIGGER IF EXISTS update_notification_preferences_updated_at ON notification_preferences;
+
 -- Trigger to automatically update updated_at
 CREATE TRIGGER update_notification_preferences_updated_at
   BEFORE UPDATE ON notification_preferences
